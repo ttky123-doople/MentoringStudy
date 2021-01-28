4006428aa7a9fccf1617f0a3d61a90f333601370
ttky123 <ttky123@doople.net>
2021-01-28 17:00:21 +0900

    01.28 C# Study Example

---
+++ Inconvinient_Null/Inconvinient_Null/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Inconvinient_Null
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string? a = "hello world";
+            string str = "a is ";
+            string temp = a ?? "null";
+            Console.WriteLine(str + temp);
+
+        }
+    }
+}
