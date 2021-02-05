c704e2616e754c40bc8809f60d0e0e192d7aeda3
ttky123 <ttky123@doople.net>
2021-02-05 16:13:51 +0900

    Mini_Project_Client

---
+++ Mini_Project/Mini_Project_Client/MiniProject_Client/MiniProject_Client/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniProject_Client
+{
+    class Person
+    {
+        private string Name { get; set; }
+        private string Phone { get; set; }
+        private string Number { get; set; }
+    }
+}
---
+++ Mini_Project/Mini_Project_Client/MiniProject_Client/MiniProject_Client/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+using System.Xml;
+using System.Net;
+using System.Threading;
+
+namespace MiniProject_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Temp
+            /*string selfIp = "MyIP";
+     string port = "MyPort";
+     string inputNum = "MyNum";
+     string inputPhone = "MyPhone";
+     string inputName = "MyName";
+     string command = "MyCommand";
+     string msg = @"<?xml version=""1.0"" encoding=""utf - 8""?>
+<Root>
+<Client>
+<IP>" + selfIp + @"</IP>
+<Port>" + port + @"</Port>
+</Client>;
+<Content>
+<Command>" + command + @"</Command>
+<Data>" + @"
+   <Student>
+      <Number>" + inputNum + @"</Number>
+      <Phone>" + inputPhone + @"</Phone>
+      <Name>" + inputName + @"</Name>
+   </Student>
+</Data>
+</Content>
+</Root>";
+     Console.WriteLine(msg);*/ 
+            #endregion
+
+            Console.WriteLine("IP");
+            string ip = Console.ReadLine();
+            Console.WriteLine("Port");
+            string port = Console.ReadLine();
+
+            Client myClient = new Client(ip, port);
+
+        }
+    }
+}
